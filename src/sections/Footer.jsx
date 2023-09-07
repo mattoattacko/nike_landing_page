2cf480320b771ae95741750612cdf114230c8aab
mattoattacko <matt.mcquain@gmail.com>
2023-09-06 17:30:41 -0700

    added customer reviews and subscribe section. Starting on the footer

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { footerLogo } from "../assets/images"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='max-container'>
+      <div className=''>
+        {/* Nike logo */}
+        <div className=''>
+          <a href='/'>
+            <img 
+              src={footerLogo}
+            />
+          </a>
+        </div>
+      </div>
+    </footer>
   )
 }
 
 export default Footer
+
+
+
+// '<footer>' is an html5 semantic tag
